76ea39b9abcdf777b255dcb5c5657ef1058a131b
casterfile <dev.anthonycastor@gmail.com>
2021-09-16 19:39:42 +0800

    first commit2

---
+++ Assets/_MyAsset/_Script/MainGame.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class MainGame : MonoBehaviour
+{
+    MyScript testScript;
+    public TMP_InputField tmpInput;
+    public Toggle togInput1;
+    public Toggle togInput2;
+    public Toggle togOutput1;
+    public Toggle togOutput2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string tmpString = @"
+        function Update()
+            if Input1 then
+                Output1 = true
+            else
+                Output1 = false
+            end
+        end
+        ";
+
+        testScript = tmpInput.GetComponentInChildren<MyScript>();
+        testScript.initScript(tmpString);
+        tmpInput.text = testScript.myScript;
+    }
+
+    // Update is called once per frame
+    public void UpdateScript()
+    {
+        string tmpString = tmpInput.text;
+        testScript.initScript(tmpString);
+        testScript.Input1 = togInput1.isOn;
+        testScript.Input2 = togInput1.isOn;
+        testScript.RunScript();
+
+        togOutput1.isOn = testScript.Output1;
+        togOutput2.isOn = testScript.Output2;
+    }
+}
---
+++ Assets/_MyAsset/_Script/MyScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MoonSharp.Interpreter;
+using System.IO;
+
+
+public class MyScript : MonoBehaviour
+{
+    public bool Input1 = false;
+    public bool Input2 = false;
+    public bool Output1 = false;
+    public bool Output2 = false;
+
+    public string myScript;
+    Script script;
+    // Start is called before the first frame update
+    public void initScript(string ScriptText)
+    {
+        script = new Script();
+        myScript = ScriptText;
+        script.DoString(myScript);
+    }
+
+    // Update is called once per frame
+    public void RunScript()
+    {
+        script.Globals["Input1"] = Input1;
+        script.Globals["Input2"] = Input2;
+        script.Globals["Output1"] = Output1;
+        script.Globals["Output2"] = Output2;
+
+        DynValue val = script.Call(script.Globals["Update"]);
+
+        Output1 = script.Globals.Get("Output1").CastToBool();
+        Output2 = script.Globals.Get("Output2").CastToBool();
+
+    }
+
+    public void initScriptFromFile(string FileName)
+    {
+        script = new Script();
+        StreamReader sr = new StreamReader(FileName);
+        string tmpString = sr.ReadToEnd();
+        sr.Close();
+        script.DoString(tmpString);
+    }
+}
